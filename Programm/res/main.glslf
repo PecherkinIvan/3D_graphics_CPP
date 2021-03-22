89d723ad59bdabb237104738ef5bb419bb867e10
TestCheat123 <dp23092002@gmail.com>
2021-03-22 17:30:03 +0700

    bug c5052 fixed
    Add VAO & VBO

--- Programm/res/main.glslf
+++
@@ -1,9 +0,0 @@
-#version 330 core
-#extension GL_ARB_separate_shader_objects : enable
-
-in vec4 a_color;
-out vec4 f_color;
-
-void main(){
-	f_color = a_color;
-}
--- Programm/src/main.cpp
+++ Programm/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-				      // Надо исправить, здесь весь код из первого видео (Где-то есть ошибка, возможно в атребутах или нет поключенной библеотеки)
-#define GLEW_STATIC  // Примечание Window, Events это классы 
+				      
+#define GLEW_STATIC  
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -11,18 +11,37 @@
 int WIDTH = 1280;
 int HEIGHT = 720;
 
+float vertices[] = { // Массив точек для треугольника
+	0.0f, 0.0f, 0.0f,
+	1.0f, 0.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+};
+
 int main() {
 	Window::initialize(WIDTH, HEIGHT, "Window 2.0");
 	Events::initialize();
 
 	glClearColor(0.6f, 0.62f, 0.65f, 1);
 
-	Shader* shader = load_shader("res/main.glslf", "res/main.glslv");
+	Shader* shader = load_shader("res/frag.glsl", "res/vert.glsl");
 	if(shader == nullptr) {
 		std::cerr << "failed to load shader" << std::endl;
 		Window::terminate();
 		return 1;
 	}
+
+	GLuint VAO, VBO; 
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(0); //Освобождение массива вершин
+
 
 	while (!Window::isShouldClose()) {
 		Events::pullEvents();
@@ -35,7 +54,15 @@ int main() {
 
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		/*Отрисовка треугольника*/
+		shader->use(); //Запуск шейдера
+		glBindVertexArray(VAO); //Запуск VAO
+		glDrawArrays(GL_TRIANGLES, 0, 3); //Запуск отрисовки массива вершин, параметры: 1) Что отрисововать | 2) Индекс элемента | 3) Кол-во вершин, которые надо отобразить0
+		glBindVertexArray(0); //Освобождение массива вершин
+		/*Конец отрисовки треугольника*/
+
 		Window::swapBuffers();
+		
 	}
 	Window::terminate();
 	return 0;
