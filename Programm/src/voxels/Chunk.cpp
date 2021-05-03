df658bcdce0802e3818bb0b8271d7122e58d0aa4
TestCheat123 <dp23092002@gmail.com>
2021-05-03 18:20:49 +0700

    landscape
    +Ландшафт через шум Перлина

--- Programm/src/voxels/Chunk.cpp
+++ Programm/src/voxels/Chunk.cpp
@@ -1,16 +1,21 @@
 #include "Chunk.h"
 #include "voxel.h"
 #include <math.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/noise.hpp>
 
-Chunk::Chunk(int xpos, int ypos, int zpos) : x(xpos), y(ypos), z(zpos) {
+Chunk::Chunk(int xpos, int ypos, int zpos) : x(xpos), y(ypos), z(zpos){
 	voxels = new voxel[CHUNK_VOL];
-	for (int y = 0; y < CHUNK_H; y++) {
-		for (int z = 0; z < CHUNK_D; z++) {
-			for (int x = 0; x < CHUNK_W; x++) {
-				int real_x = x + this->x * CHUNK_W;
+
+	for (int z = 0; z < CHUNK_D; z++){
+		for (int x = 0; x < CHUNK_W; x++){
+			int real_x = x + this->x * CHUNK_W;
+			int real_z = z + this->z * CHUNK_D;
+			float height = glm::perlin(glm::vec3(real_x*0.025f,real_z*0.025f, 0.0f));
+		 	//height += glm::perlin(glm::vec3(real_x*0.006125f,real_z*0.006125f, 0.0f))*0.5f;
+			for (int y = 0; y < CHUNK_H; y++){
 				int real_y = y + this->y * CHUNK_H;
-				int real_z = z + this->z * CHUNK_D;
-				int id = real_y <= (sin(real_x * 0.1f) * 0.5f + 0.5f) * 10;
+				int id = real_y <= (height) * 10 + 5;
 				if (real_y <= 2)
 					id = 2;
 				voxels[(y * CHUNK_D + z) * CHUNK_W + x].id = id;
@@ -19,6 +24,6 @@ Chunk::Chunk(int xpos, int ypos, int zpos) : x(xpos), y(ypos), z(zpos) {
 	}
 }
 
-Chunk::~Chunk() {
+Chunk::~Chunk(){
 	delete[] voxels;
 }
